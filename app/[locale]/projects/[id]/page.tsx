c2bb75c897e444667259ee6fff8818dcd2423dbd
duc <ducle@teqnological.asia>
2025-12-23 22:20:29 +0700

    update: revise project content and add new implement to mapping content by locale

--- app/[locale]/projects/[id]/page.tsx
+++ app/[locale]/projects/[id]/page.tsx
@@ -6,21 +6,60 @@ export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
+const Page = async ({
+  params,
+}: {
+  params: Promise<{ id: string; locale: string }>;
+}) => {
+  const { id, locale } = await params;
   const dataContent = await getDataContentMD(id);
-  console.log("dataContent ", dataContent);
   if (!dataContent) return <div>nodata</div>;
+
+  const meta = dataContent.i18n?.[locale] ?? dataContent.i18n?.vi;
+  console.log("meta ", meta);
+
+  const markdown =
+    dataContent.contentByLocale?.[locale] ??
+    dataContent.contentByLocale?.vi ??
+    "Nội dung đang được cập nhật.";
+
   return (
     <div className="content-ctn">
-      <h1 className="">{dataContent.titleVI}</h1>
-      <p className="description">{dataContent.descriptionVI}</p>
+      <h1 className="title">{meta?.title}</h1>
+      <p className="description">{meta?.description}</p>
       <div className="markdown-content">
-        <ReactMarkdown>{dataContent.content}</ReactMarkdown>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
       </div>
     </div>
   );
 };
+//   const contentVi = dataContent.content.split("<---vi--->")[1].split("<---ja--->")[0];
+//   const contentJa = dataContent.content.split("<---ja--->")[1];
+//   console.log("dataContent ", dataContent);
+
+//   return (
+//     <div className="content-ctn">
+//       {locale === "vi" ? (
+//         <>
+//           <h1 className="">{dataContent.titleVI}</h1>
+//           <p className="description">{dataContent.descriptionVI}</p>
+//         </>
+//       ) : (
+//         <>
+//           <h1 className="">{dataContent.titleJP}</h1>
+//           <p className="description">{dataContent.descriptionJP}</p>
+//         </>
+//       )}
+
+//       <div className="markdown-content">
+//         {locale === "vi" ? (
+//           <ReactMarkdown>{contentVi}</ReactMarkdown>
+//         ) : (
+//           <ReactMarkdown>{contentJa}</ReactMarkdown>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Page;
-
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,42 +1,78 @@
+// import { getDataContentMD } from "@/app/utils";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
 
+// const getDataContentMD = async (id: string) => {
+//   const projectsDir = await path.join(
+//     process.cwd(),
+//     "app",
+//     "contents",
+//     "projects"
+//   );
+
+//   const fileNames = await fs.readdirSync(projectsDir);
+
+//   let data: any[] | any[] = [];
+
+//   data = fileNames.map((fileName) => {
+//     const filePath = path.join(projectsDir, fileName);
+//     const fileContents = fs.readFileSync(filePath, "utf8");
+//     const { data, content } = matter(fileContents);
+
+//     return {
+//       id: data.id,
+//       slug: data.slug,
+//       titleVI: data.titleVI,
+//       titleJP: data.titleJP,
+//       descriptionVI: data.descriptionVI,
+//       descriptionJP: data.descriptionJP,
+//       tech: data.tech || [], // for project
+//       tag: data.tag || [], // for blog
+//       link: data.link || "", // for project
+//       date: data.date,
+//       image: data.image,
+//       content, // for project
+//     };
+//   });
+
+//   return data.find((i) => i.id === id) || null;
+// };
+
+function splitByLocale(raw: string) {
+  const result: Record<string, string> = {};
+
+  const regex = /##\s*(vi|ja)\s*\n([\s\S]*?)(?=##\s*(vi|ja)|$)/g;
+
+  let match;
+  while ((match = regex.exec(raw)) !== null) {
+    const locale = match[1];
+    const body = match[2].trim();
+    result[locale] = body;
+  }
+
+  return result;
+}
+
 const getDataContentMD = async (id: string) => {
-  const projectsDir = await path.join(
+  const filePath = path.join(
     process.cwd(),
-    "app",
-    "contents",
-    "projects"
+    "app/contents/projects",
+    `${id}.md`
   );
 
-  const fileNames = await fs.readdirSync(projectsDir);
+  if (!fs.existsSync(filePath)) return null;
 
-  let data: any[] | any[] = [];
+  const file = fs.readFileSync(filePath, "utf-8");
 
-  data = fileNames.map((fileName) => {
-    const filePath = path.join(projectsDir, fileName);
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    const { data, content } = matter(fileContents);
+  const { data, content } = matter(file);
 
-    return {
-      id: data.id,
-      slug: data.slug,
-      titleVI: data.titleVI,
-      titleJP: data.titleJP,
-      descriptionVI: data.descriptionVI,
-      descriptionJP: data.descriptionJP,
-      tech: data.tech || [], // for project
-      tag: data.tag || [], // for blog
-      link: data.link || "", // for project
-      date: data.date,
-      image: data.image,
-      content, // for project
-    };
-  });
-
-  return data.find((i) => i.id === id) || null;
+  const contentByLocale = splitByLocale(content);
+  return {
+    ...data,
+    contentByLocale,
+  };
 };
 
 export { getDataContentMD };
