3ab3c1c6d782543bd66b741faeb0a7cf4b4f172a
duc <ducle@teqnological.asia>
2025-12-30 14:54:23 +0700

    chore: add intro and intro's css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 // app/layout.tsx
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import Image from "next/image";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <div className="intro">
+          <Image src="/meo.gif" alt="meo" layout="fill" />
+        </div>
         {children}
       </body>
     </html>
